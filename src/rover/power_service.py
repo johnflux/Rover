cd7935a688097f7594a56b3c3c8f95f18d637cb6
John Tapsell <johnflux@gmail.com>
2019-12-05 14:56:42 +0900

    Improve power service

---
+++ src/rover/power_service.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import rospy
+import subprocess
+from rover.srv import PowerOff
+
+def handle_poweroff(req):
+    print("Shutting down")
+    # Note that we can use 'sudo reboot' because we did:
+    #   $ sudo visudo
+    # And added:
+    #   script  ALL=(root)  NOPASSWD: /sbin/reboot ""
+    return subprocess.call("(sleep 2; sudo reboot -h) &", shell=True)
+
+def handle_reboot(req):
+    print("Rebooting")
+    return subprocess.call("(sleep 2; sudo reboot) &", shell=True)
+
+def main():
+    rospy.init_node('power_off')
+    s_poweroff = rospy.Service('power_off/power_off', PowerOff, handle_poweroff)
+    s_reboot = rospy.Service('power_off/reboot', PowerOff, handle_reboot)
+    rospy.spin()
+
+if __name__ == "__main__":
+    main()
