09d0aa5e734541417efa2e2e5cdcefe454e0c061
John Tapsell <johnflux@gmail.com>
2019-12-05 18:11:34 +0900

    Improve print_status some more

--- print_status.py
+++ print_status.py
@@ -9,13 +9,14 @@ def main():
     def on_new_state(state):
         state_to_str = ["Idle", "Running", "Crashed", "Waiting"]
         for node in state.nodes:
-            print(node.ns, node.name, '\t', state_to_str[node.state], "\tUser:", round(100*node.user_load), "\tSys:", round(100*node.system_load), "\tMem:", round(node.memory/1024),'kb')
-        exit(0)
+            print((node.ns + ' ' + node.name).ljust(15), '\t', state_to_str[node.state], "\tUser:", str(round(100*node.user_load))+'%', "\tSys:", str(round(100*node.system_load))+'%', "  \tMem:", round(node.memory/1024),'kb')
 
     state = rospy.wait_for_message("/rosmon/state", State, 2)
     on_new_state(state)
+    print()
 
-    print("You can start/stop/restart services like:")
+    print("You can start (1), stop (2), or restart (3) services like:")
     print('  rosservice call /rosmon/start_stop "joy_drive" "" 2')
+    print()
 
 main()
