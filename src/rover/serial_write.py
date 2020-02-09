ace78231a212a05811099fc2ca21aac5452a4b05
John Tapsell <johnflux@gmail.com>
2020-02-09 13:27:56 +0900

    Control the led light when the arm moves

--- src/rover/serial_write.py
+++ src/rover/serial_write.py
@@ -16,15 +16,32 @@ def get_ip():
         s.close()
     return IP
 
+ser = serial.Serial('/dev/ttyUSB0', 115200, rtscts=False, dsrdtr=True, xonxoff=False)  # open serial port
+ser.readline()
 def send_ip():
     ip = get_ip()
     print("Ip is", ip)
-    ser = serial.Serial('/dev/ttyUSB0', 115200, rtscts=False, dsrdtr=True, xonxoff=False)  # open serial port
-    time.sleep(3)
-    ser.readline()
+    #time.sleep(3)
     ret = ser.write(ip.encode('ascii'))     # write a string
     ser.flush()
-    ser.close()             # close port
+    #ser.close()             # close port
+is_arm_led_on = False
+last_arm_led = 'o' #off
+def arm_led_on():
+    global last_arm_led, is_arm_led_on
+    if last_arm_led == 'r':
+        last_arm_led='g'
+    elif last_arm_led == 'g':
+        last_arm_led='b'
+    else:
+        last_arm_led='r'
+    is_arm_led_on = True
+    ser.write(last_arm_led.encode('ascii'))
+def arm_led_off():
+    global last_arm_led, is_arm_led_on
+    if is_arm_led_on:
+        is_arm_led_on = False
+        ser.write(b'o')
 
 if __name__ == "__main__":
     send_ip()
--- src/rover/simple_drive.py
+++ src/rover/simple_drive.py
@@ -9,7 +9,9 @@ from std_msgs.msg import Float32
 from rover.msg import Motor, Servo
 from rover.srv import PowerOff
 from sensor_msgs.msg import Joy
+from std_srvs.srv import Empty
 import os
+from . import serial_write
 
 def main():
     rospy.init_node("simple_drive")
@@ -45,6 +47,10 @@ def main():
             #print("arm updown:", data.linear.y, ", leftright:", data.linear.z)
             motors.armMiddleThrottle(arm_middle)
             motors.armBottomThrottle(arm_bottom)
+            if hand_leftright != 0:
+                serial_write.arm_led_on()
+            else:
+                serial_write.arm_led_off()
             servos.handLeftRight(hand_leftright)
             servos.handUpDown(hand_updown)
         except Exception as e:
@@ -110,8 +116,8 @@ def main():
         arm_middle = data.axes[0] / speed_setting # horiz
         arm_bottom = data.axes[1] / speed_setting # vert
 
-        hand_leftright = data.axes[6]
-        hand_updown = data.axes[7]
+        hand_leftright = data.axes[6] * 5
+        hand_updown = data.axes[7] * 5
 
         # Publish Twist
         twist = Twist()
@@ -130,9 +136,12 @@ def main():
             quadcopter_cover_value = 1.0 if quadcopter_cover else 0.0
             #quadcopter_cover_pub.publish( 1.0 if quadcopter_cover else 0.0 )
             on_new_quadcopter_cover(quadcopter_cover_value)
+        if data.buttons[0]:
+            on_display_ip_on_leds()
 
     def on_display_ip_on_leds():
         os.system("~/rover/src/rover/serial_write.py")
+        rospy.loginfo("IP Address is " + serial_write.get_ip())
 
     subscriber_twist = rospy.Subscriber("cmd_vel", Twist, on_new_twist_body, queue_size=10)
     subscriber_servo = rospy.Subscriber("quadcopter_cover", Float32, on_new_quadcopter_cover, queue_size=10)
@@ -140,6 +149,8 @@ def main():
 
     subscriber_motor_cmd = rospy.Subscriber("motor_cmd", Motor, on_new_raw_motor, queue_size=1)
     subscriber_servo_cmd = rospy.Subscriber("servo_cmd", Servo, on_new_raw_servo, queue_size=1)
+
+    service_displayip = rospy.Service('display_ip', Empty, on_display_ip_on_leds)
 
     on_display_ip_on_leds()
 
